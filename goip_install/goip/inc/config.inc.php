06047ae190aaa7dd73a8005aba20a725f967d1f8
Roman <toxi@ua.fm>
2019-04-15 23:51:35 +0300

    goip_install-v1.24.4
    goip_install-v1.24.4

---
+++ goip_install/goip/inc/config.inc.php
@@ -0,0 +1,10 @@
+<?php 
+$dbhost='localhost';	//database server 
+$dbuser='goip';		//database username 
+$dbpw='goip';		//database password 
+$dbname='goip';		//database name
+$goipcronport='44444';  //goipcron port
+$charset='utf8';
+$endless_send=0;
+$re_ask_timer=3;
+?>
